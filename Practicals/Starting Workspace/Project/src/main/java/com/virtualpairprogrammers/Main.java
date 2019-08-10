b3156f3e19542419dfa9fb5566b26f08e6bcf88d
jcorcoranie <jcorcoranie@aol.com>
2019-08-10 14:21:52 +0100

    pairRDDs samples

--- Practicals/Starting Workspace/Project/src/main/java/com/virtualpairprogrammers/Main.java
+++ Practicals/Starting Workspace/Project/src/main/java/com/virtualpairprogrammers/Main.java
@@ -3,6 +3,7 @@ package com.virtualpairprogrammers;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
@@ -14,54 +15,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Double> inputData = new ArrayList<>();
-        inputData.add(35.5);
-        inputData.add(12.49943);
-        inputData.add(90.32);
-        inputData.add(20.32);
+        List<String> inputData = new ArrayList<>();
+        inputData.add("Warn: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("Warn: Saturday 8 September 1942");
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+        JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
 
-        Double result = myRdd.reduce((value1, value2) -> value1 + value2);
-        System.out.println("\nResult = " + result);
+        JavaPairRDD<String, Long> pairRDD = originalLogMessages.mapToPair( rawData -> new Tuple2<>(rawData.split(":")[0], 1L));
 
-        System.out.println("======================\n");
+        JavaPairRDD<String, Long> sumsbyKeyRdd = pairRDD.reduceByKey((value1, value2) -> value1 + value2);
 
+        sumsbyKeyRdd.foreach( tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances."));
 
-        List<Integer> intInputData = new ArrayList<>();
-        intInputData.add(35);
-        intInputData.add(12);
-        intInputData.add(90);
-        intInputData.add(20);
+        System.out.println();
+        System.out.println("===========================================");
+        System.out.println();
 
-        JavaRDD<Integer> myIntRdd = sc.parallelize(intInputData);
-
-        JavaRDD<Double> sqrtRdd = myIntRdd.map( value -> Math.sqrt(value));
-
-        sqrtRdd.foreach( value -> System.out.println(value));
-        System.out.println("======================\n");
-        sqrtRdd.collect().forEach( System.out::println);
-
-        JavaRDD<Integer> myCountRdd = myRdd.map( value -> 1);
-        int count = myCountRdd.reduce((val1,val2) -> val1 + val2);
-
-        System.out.println("======================\n");
-        System.out.println("Count = " + count);
-
-        System.out.println("======================\n");
-
-        JavaRDD<IntegerWithSquareRoot> sqrtRdd2 = myIntRdd.map( value -> new IntegerWithSquareRoot(value));
-
-        sqrtRdd2.foreach(value -> System.out.println("Square root for " + value.getOriginalNumber() + " is " + value.getSquareRoot()));
-
-        System.out.println("======================\n");
-
-        Tuple2<Integer, Double> myTupleValue = new Tuple2<> (9, 3.0);
+        sc.parallelize(inputData)
+                .mapToPair( rawData -> new Tuple2<>(rawData.split(":")[0], 1L))
+                .reduceByKey((value1, value2) -> value1 + value2)
+                .foreach( tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances."));
 
         sc.close();
     }
